3d8fefe053f8cfe0a72ce5dc41aa51d5ca7a6f92
Ajay S <ajaycshan@gmail.com>
2023-11-21 00:25:35 +1100

    Add associations between models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    // Disables unique constraint (i.e. foreign key of the ProductTag model does not need to be unique)
+    unique: false
+  },
+  as: 'product_tags'
+})
 
-// Tags belongToMany Products (through ProductTag)
+// TODO: Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    ProductTag,
+    unique: false
+  },
+  as: 'tagged_products'
+})
 
 module.exports = {
   Product,
