aaebb3d97f6e3198eca21ace86e448092dc7e139
Ajay S <ajaycshan@gmail.com>
2023-11-22 00:42:29 +1100

    Install mysql2

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -43,7 +43,7 @@ router.post('/', (req, res) => {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
-      tagIds: [1, 2, 3, 4]
+      tagId: [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
